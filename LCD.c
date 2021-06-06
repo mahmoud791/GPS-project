7c7251fefa342b8b5dc2f8507a2c8d1d27a76f6f
mostafawagih <73798262+mostafawagih@users.noreply.github.com>
2021-06-06 20:35:30 +0200

    Add files via upload

---
+++ LCD.c
@@ -0,0 +1,148 @@
+#include "LCD.h"
+#include "systick.h"
+#include <stdlib.h>
+#include "common_macros.h"
+
+
+
+void delay_ms(unsigned long long n)
+{
+    volatile unsigned long long i = 0;
+    while(i++ < (1600 * n) );
+
+}
+
+
+
+
+
+
+
+
+void LCD_sendCommand(uint8 command)
+{
+    CLEAR_BIT(GPIO_PORTA_DATA_REG, RW);
+    CLEAR_BIT(GPIO_PORTA_DATA_REG,RS);
+    delay_ms(2);
+    SET_BIT(GPIO_PORTA_DATA_REG,E);
+    delay_ms(2);
+    GPIO_PORTB_DATA_REG = command;
+    delay_ms(2);
+    CLEAR_BIT(GPIO_PORTA_DATA_REG,E);
+    delay_ms(2);
+
+}
+
+
+
+
+
+static void LCD_Dataport_init (void){
+
+    SYSCTL_REGCGC2_REG |= (1<<1); //TURN UART CLOCK
+    while(!(SYSCTL_PRGPIO_REG & (1<<1)));
+    GPIO_PORTB_DIR_REG = 0XFFFFFFFF;
+    GPIO_PORTB_AFSEL_REG = 0;
+    GPIO_PORTB_AMSEL_REG = 0;
+    GPIO_PORTB_DEN_REG = 0XFFFFFFFF;
+    GPIO_PORTB_PCTL_REG = 0;
+
+}
+
+
+static void LCD_ControlPort_init (void){
+
+    SYSCTL_REGCGC2_REG |= (1<<0);
+    while(!(SYSCTL_PRGPIO_REG & (1<<0)));
+    GPIO_PORTA_DIR_REG |= (0X1C);
+    GPIO_PORTA_AFSEL_REG &= ~(0X1C);  // 0001 1100
+    GPIO_PORTA_AMSEL_REG &= ~(0X1C);
+    GPIO_PORTA_DEN_REG |= (0X1C); // 0001 1100
+    GPIO_PORTA_PCTL_REG &= ~(0X1C);
+
+}
+
+void LCD_init (void)
+{
+
+    LCD_Dataport_init();
+    LCD_ControlPort_init();
+
+
+
+
+    LCD_sendCommand(twoLines8BitMode);
+
+    LCD_sendCommand(cursorOff);
+
+    LCD_sendCommand(clearScreen);
+}
+
+
+
+
+
+void LCD_displayCharacter (uint8 Char)
+{
+    CLEAR_BIT(GPIO_PORTA_DATA_REG, RW);
+    SET_BIT(GPIO_PORTA_DATA_REG,RS);
+    delay_ms(2);
+    SET_BIT(GPIO_PORTA_DATA_REG,E);
+    delay_ms(2);
+    GPIO_PORTB_DATA_REG = Char;
+    delay_ms(2);
+    CLEAR_BIT(GPIO_PORTA_DATA_REG,E);
+    delay_ms(2);
+
+}
+
+void LCD_displayString (sint8* str){
+    uint8 i=0;
+    while(str[i] != '\0')
+    {
+        LCD_displayCharacter(str[i]);
+        i++;
+    }
+}
+
+
+void LCD_goToRowColumn(uint8 row,uint8 col){
+    uint8 address;
+    switch (row){
+    case 0:
+        address = col;
+        break;
+    case 1:
+        address = col + 0X40;
+        break;
+    case 2:
+        address = col + 0X10;
+        break;
+    case 3:
+        address = col + 0X50;
+        break;
+    }
+
+    LCD_sendCommand(address | SetCursorLocation);
+}
+
+void LCD_displayStringRowColumn(uint8 row,uint8 col,sint8* str){
+
+    LCD_goToRowColumn(row, col);
+    LCD_displayString(str);
+
+}
+
+void LCD_clearScreen (void){
+    LCD_sendCommand(clearScreen);
+}
+
+
+void LCD_intgerToStringDisplay (int data){
+
+    sint8 str [15];
+    ltoa(data,str,10);
+    LCD_displayString(str);
+
+}
+
---
+++ LCD.h
@@ -0,0 +1,60 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "typedef.h"
+#include "common_macros.h"
+#include "tm4c123gh6pm_registers.h"
+
+
+
+
+
+#define LCD_ControlDirection GPIO_PORTA_DIR_REG
+#define LCD_ControlData GPIO_PORTA_DATA_REG
+#define RS  2
+#define RW 3
+#define E 4
+
+#define LCD_dataPort GPIO_PORTB_DATA_REG
+#define LCD_dataPortDir GPIO_PORTB_DIR_REG
+
+
+#define clearScreen  0X01
+#define cursorOff 0X0C
+#define cursorOn 0X0E
+#define twoLines8BitMode 0X38
+#define SetCursorLocation 0X80
+#define shiftCursorPositionRight 0x14
+#define wakeUp 0X30
+#define incrment 0x06
+#define startDisplay 0x0F
+
+
+void delay_ms(unsigned long long n);
+
+void LCD_init(void);
+
+static void LCD_ControlPort_init (void);
+
+static void LCD_Dataport_init (void);
+
+void LCD_sendCommand (uint8);
+
+void LCD_displayCharacter (uint8);
+
+void LCD_displayString (sint8*);
+
+void LCD_goToRowColumn(uint8,uint8);
+
+void LCD_displayStringRowColumn(uint8,uint8,sint8*);
+
+void LCD_clearScreen (void);
+
+void LCD_intgerToStringDisplay (int);
+
+
+
+
+#endif
