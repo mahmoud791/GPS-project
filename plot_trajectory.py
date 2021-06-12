a7f7b5734b1fde4ef59610755ee6fff7c65128bd
Mahmod 3ashry <64699750+mahmoud791@users.noreply.github.com>
2021-06-12 16:28:03 +0200

    Create plot_trajectory.py

---
+++ plot_trajectory.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+s = input("enter file path :")
+path = s.replace('\\', '/')
+
+
+
+with open(path) as f:
+    content = f.readlines()
+
+LON = []
+LAT = []
+
+for line in content:
+    if line[0]=='=':
+        continue
+    if line == '\n':
+        continue
+
+    if line == '0':
+        continue
+
+    line1 = line.split(sep=',')
+
+    lon_string = line1[0].split()
+
+    lat_string = line1[1].split()
+
+    
+
+    longitude = int(lon_string[0]) + float(int(lon_string[1])/60) +  float(int(lon_string[2])/6000000)
+    latitude = int(lat_string[0]) + float(int(lat_string[1])/60) +  float(int(lat_string[2])/6000000)
+    LON.append(longitude)
+    LAT.append(latitude)
+
+origin_x = LAT[0]
+origin_y = LON[0]
+
+for i in range(len(LAT)):
+    LAT[i] -= origin_x
+
+for i in range(len(LON)):
+    LON[i] -= origin_y
+    
+
+
+    
+
+plt.plot(LAT,LON)
+plt.show()
