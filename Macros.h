85a76c37559445fd57fff443ee4de8f26ff215b1
mostafawagih <73798262+mostafawagih@users.noreply.github.com>
2021-06-12 21:17:35 +0200

    Update Macros.h

--- Macros.h
+++ Macros.h
@@ -4,18 +4,18 @@
 //MACROS definition used in project
 
 
-#define SET_BIT(REG,BIT) (REG|=(1<<BIT))
+#define SET_BIT(REG,BIT) (REG|=(1<<BIT))                //Macro to set bit in register
 
-#define CLEAR_BIT(REG,BIT) (REG&=(~(1<<BIT)))
+#define CLEAR_BIT(REG,BIT) (REG&=(~(1<<BIT)))                //Macro to clear bit in register
 
-#define TOGGLE_BIT(REG,BIT) (REG^=(1<<BIT))
+#define TOGGLE_BIT(REG,BIT) (REG^=(1<<BIT))                //Macro to toggle bit in register
 
-#define ROR(REG,num) ( REG= (REG>>num) | (REG<<(8-num)) )
+#define ROR(REG,num) ( REG= (REG>>num) | (REG<<(8-num)) )                           //Macro to rotate right in register
 
-#define ROL(REG,num) ( REG= (REG<<num) | (REG>>(8-num)) )
+#define ROL(REG,num) ( REG= (REG<<num) | (REG>>(8-num)) )                           //Macro to rotate left in register
 
-#define BIT_IS_SET(REG,BIT) ( REG & (1<<BIT) )
+#define BIT_IS_SET(REG,BIT) ( REG & (1<<BIT) )              //Macro to check if bit is set
 
-#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+#define BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )              //Macro to check if bit is cleared
 
 #endif
